e56d7e35033cc5fe864949d0904c037ee9b5fdd2
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-01-28 15:59:07 +0200

    add spinner refrence

--- Evaluation/src/features/dispalyer/components/InfiniteList.tsx
+++ Evaluation/src/features/dispalyer/components/InfiniteList.tsx
@@ -1,5 +1,7 @@
 import ListItem from "./ListItem";
 import type { Breed } from "../types/breeds";
+import Spinner from "@/components/common/Spinner";
+import { useEffect, useRef, useState } from "react";
 
 type InfiniteListProps = {
   items: Breed[];
@@ -11,13 +13,47 @@ export default function InfiniteList({
   isLoading,
   error,
 }: InfiniteListProps) {
-  if (isLoading) return <p>...loading</p>;
-  if (error) return <p>!error</p>;
+  const spinnerRef = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1,
+      },
+    );
+    if (spinnerRef.current) {
+      observer.observe(spinnerRef.current);
+    }
+    return () => {
+      if (spinnerRef.current) observer.unobserve(spinnerRef.current);
+    };
+  }, [spinnerRef]);
+  console.log(isIntersecting);
   return (
-    <ul className="mt-16 flex justify-between items-center flex-col gap-1">
-      {items.map((breed: Breed) => (
-        <ListItem key={breed.id}>{breed.attributes?.name}</ListItem>
-      ))}
-    </ul>
+    <div className="flex py-16 justify-between items-center flex-col my-auto">
+      {isLoading ? (
+        <Spinner />
+      ) : error ? (
+        <p>!error</p>
+      ) : (
+        <div>
+          {
+            <ul className=" flex justify-between items-center flex-col gap-1">
+              {items.map((breed: Breed) => (
+                <ListItem key={breed.id}>{breed.attributes?.name}</ListItem>
+              ))}
+            </ul>
+          }
+        </div>
+      )}
+      <div ref={spinnerRef} className="mt-16">
+        <Spinner />
+      </div>
+    </div>
   );
 }
